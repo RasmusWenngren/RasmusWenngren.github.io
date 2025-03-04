1e8e38af20ed175c10ed9958d2ccab1d0c1801ab
Rasemouse <rasmuswenngren@gmail.com>
2025-03-04 11:39:19 +0100

    Fixed Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,6 +7,40 @@ import {
 import { faPaperPlane, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import PageLayout from "../components/PageLayout";
 
+const contactData = [
+  {
+    title: "Email",
+    icon: faEnvelope,
+    link: "mailto:rasmuswenngren@gmail.com?subject=Don't forget to add a subject!&body=Feel free to start a conversation about whatever you like!",
+    description:
+      "Send me an email, and I'll get back to you as soon as possible!",
+  },
+  {
+    title: "GitHub",
+    icon: faGithub,
+    link: "https://github.com/RasmusWenngren92",
+    description: "Check out my projects and contributions on GitHub!",
+  },
+  {
+    title: "Twitter (X)",
+    icon: faTwitter,
+    link: "https://x.com/rasemouse",
+    description: "Follow me on X (formerly Twitter) to stay updated!",
+  },
+  {
+    title: "LinkedIn",
+    icon: faLinkedin,
+    link: "https://www.linkedin.com/in/rasmus-wenngren-a80834269",
+    description: "Connect with me on LinkedIn and let's network!",
+  },
+  {
+    title: "WhatsApp",
+    icon: faPaperPlane,
+    link: "https://wa.me/+46709248696",
+    description: "Send me a message on WhatsApp for a quick chat!",
+  },
+];
+
 export default function Contact() {
   return (
     <>
@@ -24,34 +58,17 @@ export default function Contact() {
           you prefere.
         </p>
 
-        
-          <div className="contact-info">
-            <a
-              href="mailto:rasmuswenngren@gmail.com?subject=Don't forger to add a subject!&body=Feel free to start  conversation about whaterver you like!?"
-              aria-label="Email me"
-            >
-              <FontAwesomeIcon icon={faEnvelope} />
-            </a>
-            <a href="https://github.com/RasmusWenngren92" aria-label="GitHub">
-              <FontAwesomeIcon icon={faGithub} />
-            </a>
-            <a href="https://x.com/rasemouse" aria-label="X former Twitter">
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/rasmus-wenngren-a80834269/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BFgQb%2B93ST0G8ycoG83SzzA%3D%3D"
-              aria-label="LinkedIn"
-            >
-              <FontAwesomeIcon icon={faLinkedin} />
-            </a>
-            <a href="https://wa.me/+46709248696">
-              <FontAwesomeIcon
-                icon={faPaperPlane}
-                aria-label="Send Message on WhatsApp"
-              />
-            </a>
-          </div>
-        
+        <section className="grid">
+          {contactData.map((contact, index) => (
+            <Card
+              key={index}
+              title={contact.title}
+              description={contact.description}
+              icon={<FontAwesomeIcon icon={contact.icon} size="2x" />}
+              link={contact.link}
+            />
+          ))}
+        </section>
       </PageLayout>
     </>
   );
